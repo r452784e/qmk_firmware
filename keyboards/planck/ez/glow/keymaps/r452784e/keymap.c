0b6ba3658e35ef859dffab1b5c8cbe8929069cf0
r452784e <pochi.12399@gmail.com>
2022-11-30 21:21:00 +0900

    update keymap

--- keyboards/planck/ez/glow/keymaps/r452784e/keymap.c
+++ keyboards/planck/ez/glow/keymaps/r452784e/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPACE,
         KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
         KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_ENTER,
-        KC_LCTRL,       KC_LALT,        KC_LGUI,        TD(DANCE_0),    LOWER,          KC_SPACE,       KC_NO,          RAISE,          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT
+        KC_LCTRL,       TD(DANCE_0),    KC_LALT,        KC_LGUI,        LOWER,          KC_SPACE,       KC_NO,          RAISE,          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT
         ),
 
     [_LOWER] = LAYOUT_planck_grid(
@@ -222,30 +222,30 @@ void dance_0_reset(qk_tap_dance_state_t *state, void *user_data);
 
 void on_dance_0(qk_tap_dance_state_t *state, void *user_data) {
     if(state->count == 3) {
-        tap_code16(KC_LANG1);
-        tap_code16(KC_LANG1);
-        tap_code16(KC_LANG1);
+        tap_code16(KC_LANG2);
+        tap_code16(KC_LANG2);
+        tap_code16(KC_LANG2);
     }
     if(state->count > 3) {
-        tap_code16(KC_LANG1);
+        tap_code16(KC_LANG2);
     }
 }
 
 void dance_0_finished(qk_tap_dance_state_t *state, void *user_data) {
     dance_state[0].step = dance_step(state);
     switch (dance_state[0].step) {
-        case SINGLE_TAP: register_code16(KC_LANG1); break;
-        case DOUBLE_TAP: register_code16(KC_LANG2); break;
-        case DOUBLE_SINGLE_TAP: tap_code16(KC_LANG1); register_code16(KC_LANG1);
+        case SINGLE_TAP: register_code16(KC_LANG2); break;
+        case DOUBLE_TAP: register_code16(KC_LANG1); break;
+        case DOUBLE_SINGLE_TAP: tap_code16(KC_LANG2); register_code16(KC_LANG2);
     }
 }
 
 void dance_0_reset(qk_tap_dance_state_t *state, void *user_data) {
     wait_ms(10);
     switch (dance_state[0].step) {
-        case SINGLE_TAP: unregister_code16(KC_LANG1); break;
-        case DOUBLE_TAP: unregister_code16(KC_LANG2); break;
-        case DOUBLE_SINGLE_TAP: unregister_code16(KC_LANG1); break;
+        case SINGLE_TAP: unregister_code16(KC_LANG2); break;
+        case DOUBLE_TAP: unregister_code16(KC_LANG1); break;
+        case DOUBLE_SINGLE_TAP: unregister_code16(KC_LANG2); break;
     }
     dance_state[0].step = 0;
 }
